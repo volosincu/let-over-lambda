51a5db8c7da0582a71805acf6619614d5896b6bb
volosincu <bogdan.volosincu@gmail.com>
2016-12-24 22:30:06 +0000

    v.2.0 group

--- group.lisp
+++ group.lisp
@@ -1,28 +1,14 @@
 
 
 (defun group (lst by)
-  (let ((ll '())
-	(buff '()))
-    (labels ((rec (l)
-	       (let ((head (car l))
-		     (tail (cdr l)))
-		 (cond (head
-			(if (eq (length buff) by)
-			    (progn
-			      (push (reverse buff) ll)
-			      (setq buff '())
-			      (push head buff))
-			    (push head buff))
-			(if (null tail)
-			    (push buff ll)
-			    (rec tail))
-			)))))
-      (rec lst))
-    (reverse ll)))
-
-
-;(defun ggroup (lst by)
-;  (let ((f (car lst)))
-;	(if(null f)
-;	   nil
-;	   (progn (print f) (ggroup (cdr lst))))))
+  (let ((tuple '()))
+  (labels ((rec (xs)
+	     (if (not (null xs))
+		 (if (>= (length xs) by)
+		     (progn
+		       (push (subseq xs 0 by) tuple)
+		       (rec (subseq xs by)))
+		     (push xs tuple)))
+	     ))	       
+    (rec lst))
+  (reverse tuple)))
