0b83db377836c0b2f0ab145aac0b96e08e445361
volosincu <bogdan.volosincu@gmail.com>
2016-12-25 14:22:58 +0000

    v.3.0 group

--- group.lisp
+++ group.lisp
@@ -1,14 +1,11 @@
 
 
 (defun group (lst by)
-  (let ((tuple '()))
   (labels ((rec (xs)
 	     (if (not (null xs))
 		 (if (>= (length xs) by)
-		     (progn
-		       (push (subseq xs 0 by) tuple)
-		       (rec (subseq xs by)))
-		     (push xs tuple)))
-	     ))	       
-    (rec lst))
-  (reverse tuple)))
+		     (cons (subseq xs 0 by) (rec (nthcdr by xs))) 
+		     (cons xs nil) )
+		 )))
+    (rec lst)))
+
